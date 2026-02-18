3d4fc97d25a00f46a5ee2063f7f7bc623e8eaad6
Aliza Jane <aliza.tacbobo@lccdo.edu.ph>
2026-02-18 15:45:20 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const btn = document .getElementById("press"); 
+let header1 = document.getElementById("header1")
+
+
+btn.addEventListener("click", function(){
+    header1.textContent = " hello everyone, WELCOME!";
+});
